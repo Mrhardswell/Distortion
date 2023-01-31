114fe2d96f3084c5950c097b244546ba6e3914a7
MrHardswell <50495373+Mrhardswell@users.noreply.github.com>
2023-01-31 17:11:36 -0400

    Stable Release

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,8 @@
-print("Hello world, from server!")
+local RS = game:GetService("ReplicatedStorage")
+local Knit = require(RS.Packages.Knit)
+
+assert(Knit, "Knit not found. Make sure it is installed into ReplicatedStorage.")
+
+Knit.Start():andThen(function()
+    print("Knit started!")
+end):catch(warn)
